8a3b80cb296fc917e3069fece58b5946b6f66a0b
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-07-06 21:33:32 -0500

    codigos qiue encontré en rubyfu

---
+++ codigos de prueba/colores.rb
@@ -0,0 +1,28 @@
+class String
+  def red; colorize(self, "\e[1m\e[31m"); end
+  def green; colorize(self, "\e[1m\e[32m"); end
+  def dark_green; colorize(self, "\e[32m"); end
+  def yellow; colorize(self, "\e[1m\e[33m"); end
+  def blue; colorize(self, "\e[1m\e[34m"); end
+  def dark_blue; colorize(self, "\e[34m"); end
+  def purple; colorize(self, "\e[35m"); end
+  def dark_purple; colorize(self, "\e[1;35m"); end
+  def cyan; colorize(self, "\e[1;36m"); end
+  def dark_cyan; colorize(self, "\e[36m"); end
+  def pure; colorize(self, "\e[0m\e[28m"); end
+  def bold; colorize(self, "\e[1m"); end
+  def colorize(text, color_code) "#{color_code}#{text}\e[0m" end
+end
+
+puts "Hola mundo".red
+puts "Hola mundo".green
+puts "Hola mundo".dark_green
+puts "Hola mundo".yellow
+puts "Hola mundo".blue
+puts "Hola mundo".dark_blue
+puts "Hola mundo".purple
+puts "Hola mundo".dark_purple
+puts "Hola mundo".cyan
+puts "Hola mundo".dark_cyan
+puts "Hola mundo".pure
+puts "Hola mundo".bold
---
+++ codigos de prueba/tiempo.rb
@@ -0,0 +1,4 @@
+(1..5).to_a.reverse.each do |c|
+  print "\rI'll exit after #{c} second(s)"
+  sleep 1
+end
